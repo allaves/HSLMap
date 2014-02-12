ace2d662fa3acca45820f4da26ae5599a3f5c04f
Alejandro Llaves <allaves@delicias.dia.fi.upm.es>
2014-02-12 19:02:18 +0100

    Map of Helsinki showing a moving bus marker.

---
+++ HSLMap/src/websockets/EchoEndpoint.java
@@ -0,0 +1,31 @@
+package websockets;
+
+import java.io.IOException;
+
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpointConfig;
+
+public class EchoEndpoint extends Endpoint{
+
+	@Override
+	public void onOpen(final Session session, EndpointConfig config) {
+		session.addMessageHandler(new MessageHandler.Whole<String>() {
+
+			@Override
+			public void onMessage(String msg) {
+				try {
+					session.getBasicRemote().sendText(msg);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+}
+
+
